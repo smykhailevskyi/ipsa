9813c2faa3a90d6477d439ae9f97c4ef58083f53
Serhii Mykhailevskyi <serhii.mykhailevskyi@gmail.com>
2021-10-26 20:27:55 +0300

    Implement getting values from console

--- algo_and_prog/lab1/lab.c
+++ algo_and_prog/lab1/lab.c
@@ -1,8 +1,17 @@
 #include "utils.c"
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  float result = culc(2, 3);
+int main(int argc, char *argv[]) {
+  if(argc != 3) {
+    printf("Please set up first and second values \n");
+    return -1;
+  }
+
+  float firstArg = atoi(argv[1]);
+  float secondArg = atoi(argv[2]);
+
+  float result = culc(firstArg, secondArg);
   printf("Result is %.2f \n", result);
 
   return 0;
